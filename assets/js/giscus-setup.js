85d12c6ffa430b367a0cac2ab6c120f159a08a1d
gujialiang123 <54051951+gujialiang123@users.noreply.github.com>
2026-04-29 05:58:44 +0000

    Deploying to gh-pages from @ gujialiang123/gujialiang123.github.io@b8240dcf90857b3ddba1d7ff91440ff77a7c0490 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "gujialiang123/NewHomepage",
+    "data-repo": "gujialiang123/gujialiang123.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
